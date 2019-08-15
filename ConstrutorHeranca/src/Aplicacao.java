445b11512803f59a534c7056791f50b2255ff8ac
Bruno Araújo <50248497+hjamesp@users.noreply.github.com>
2019-08-15 10:01:15 -0300

    Adicionando arquivos

---
+++ ConstrutorHeranca/src/Aplicacao.java
@@ -0,0 +1,11 @@
+
+public class Aplicacao {
+	
+	public static void main(String[] args) {
+		
+		Carro c = new Carro("Ferrari");
+		
+		
+	}
+
+}
---
+++ ConstrutorHeranca/src/Veiculo.java
@@ -0,0 +1,16 @@
+
+public class Veiculo {
+	
+	protected String marca;
+	
+	public Veiculo() {
+	super();
+	System.out.println("Veiculo()");
+	}
+	
+	public Veiculo(String marca) {
+		this.marca = marca;
+		System.out.println("Veiculo(String)");
+		
+	}
+}
